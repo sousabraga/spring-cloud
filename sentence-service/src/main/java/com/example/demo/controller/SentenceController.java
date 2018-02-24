fb586838f25ff0d56003157885e1da2066e538da
Matheus Braga <matheusssbraga@gmail.com>
2018-02-24 18:55:49 -0300

    Refatorando o Sentence Service para utilizar o Ribbon como Client Side Load Balancer

--- sentence-service/src/main/java/com/example/demo/controller/SentenceController.java
+++ sentence-service/src/main/java/com/example/demo/controller/SentenceController.java
@@ -1,11 +1,6 @@
 package com.example.demo.controller;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class SentenceController {
 
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	private RestTemplate restTemplate;
 
 	@GetMapping("/sentence")
 	public @ResponseBody String getSentence() {
@@ -24,16 +19,7 @@ public class SentenceController {
 	}
 
 	public String getWord(String service) {
-		List<ServiceInstance> list = discoveryClient.getInstances(service);
-		
-		if (list != null && list.size() > 0) {
-			URI uri = list.get(0).getUri();
-		
-			if (uri != null) 
-				return (new RestTemplate()).getForObject(uri, String.class);
-		}
-		
-		return null;
+		return restTemplate.getForObject("http://" + service, String.class);	
 	}
 
 }
